d4a8ff6dfe8fb40133f6fcaef78bb069ede7d655
Patrik Spathon <patrik@spathon.com>
2014-04-04 14:45:11 +0200

    Add/remove items, show current sql

--- js/JoinsCtrl.js
+++ js/JoinsCtrl.js
@@ -1,3 +1,5 @@
+var joinApp = angular.module('joinApp', ['ngSanitize']);
+
 
 
 function JoinsCtrl($scope) {
@@ -19,13 +21,24 @@ function JoinsCtrl($scope) {
     { user_id: 4, name: 'Apples' }
   ];
 
+  $scope.sql = {
+    inner: "SELECT users.name AS name, likes.name AS 'like' FROM users JOIN likes ON users.id = likes.user_id;",
+    left: "SELECT users.name AS name, likes.name AS 'like' FROM users LEFT JOIN likes ON users.id = likes.user_id;",
+    right: "SELECT users.name AS name, likes.name AS 'like' FROM users RIGHT JOIN likes ON users.id = likes.user_id;",
+    outer:
+      "SELECT users.name AS 'name', likes.name AS 'like' FROM users LEFT OUTER JOIN likes ON users.id = likes.user_id"+
+      "<br>UNION"+
+      "<br>SELECT users.name AS 'name', likes.name AS 'like' FROM users RIGHT OUTER JOIN likes ON users.id = likes.user_id"
+  }
+
   $scope.joins = [];
   $scope.user_ids = [];
   $scope.current_join = 'inner';
+  $scope.type = false;
 
-  $scope.isSelected = function(id){
-    if($scope.user_ids.indexOf(id) >= 0){
-      return 'is-selected';
+  $scope.isNotSelected = function(id){
+    if($scope.user_ids.indexOf(id) === -1){
+      return 'is-not-selected';
     }
   }
 
@@ -39,6 +52,29 @@ function JoinsCtrl($scope) {
   $scope.selectJoin = function(join){
     $scope.current_join = join;
     $scope[join +'Join']();
+  };
+
+  $scope.removeItem = function(type, index){
+    $scope[type].splice(index, 1);
+    $scope.selectJoin($scope.current_join);
+  };
+
+  $scope.addItem = function(){
+
+    // add the item to the array
+    var newItem = { name: $scope.addName };
+    newItem[($scope.type == 'users') ? 'id' : 'user_id'] = $scope.addId;
+    $scope[$scope.type].push( newItem );
+
+    // Clear the data
+    $scope.addName = '';
+    $scope.addId = '';
+
+    // recreate the join table
+    $scope.selectJoin($scope.current_join);
+
+    // Close the modal
+    $scope.moduleType = false;
   };
 
 
@@ -88,6 +124,7 @@ function JoinsCtrl($scope) {
   };
 
 
+  // SELECT users.name AS name, likes.name AS 'like' FROM users RIGHT JOIN likes ON users.id = likes.user_id;
   $scope.rightJoin = function(){
 
     var result = [];
