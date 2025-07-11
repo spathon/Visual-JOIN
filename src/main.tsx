c50da62ab9ca031bae025ca987b9f8855eed7974
Patrik Spathon <patrik@spathon.com>
2025-07-11 18:08:38 +0200

    Fix double css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import JoinsApp from './JoinsApp';
-import './styles.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
--- src/styles.d.ts
+++
@@ -1,3 +0,0 @@
-// Add type definitions for importing CSS in TypeScript
-// This is a workaround for the error: Cannot find module './styles.css' or its corresponding type declarations.
-declare module '*.css';
