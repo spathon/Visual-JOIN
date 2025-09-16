18449aeb21e69aa0cc4ffe279c7b1676c66a63e5
spathon <patrik@spathon.com>
2025-09-16 15:25:16 +0200

    Update packages

--- src/JoinSVG.tsx
+++ src/JoinSVG.tsx
@@ -1,14 +1,14 @@
-const width = 112.5;
-const height = 75;
-const centerY = height / 2;
-const margin = 1; // for stroke
-const centerX1 = 37.5; // 1/3 of width
-const centerX2 = 75;   // 2/3 of width
-const radius = Math.min(centerX1, height / 2) - margin; // maximize radius to fill viewBox
-const fillColor = '#CC333F';
-const strokeColorLeft = '#00A0B0';
-const strokeColorRight = '#EB6841';
-const strokeWidth = 1.5;
+const width = 112.5
+const height = 75
+const centerY = height / 2
+const margin = 1 // for stroke
+const centerX1 = 37.5 // 1/3 of width
+const centerX2 = 75 // 2/3 of width
+const radius = Math.min(centerX1, height / 2) - margin // maximize radius to fill viewBox
+const fillColor = '#CC333F'
+const strokeColorLeft = '#00A0B0'
+const strokeColorRight = '#EB6841'
+const strokeWidth = 1.5
 
 // Simple Venn-style SVGs for JOIN types
 export const InnerJoinSVG = () => {
@@ -31,10 +31,24 @@ export const InnerJoinSVG = () => {
         <circle cx={centerX1} cy={centerY} r={radius} fill={fillColor} />
       </g>
       {/* Borders always on top */}
-      <circle cx={centerX1} cy={centerY} r={radius} fill="none" stroke={strokeColorLeft} strokeWidth={strokeWidth} />
-      <circle cx={centerX2} cy={centerY} r={radius} fill="none" stroke={strokeColorRight} strokeWidth={strokeWidth} />
+      <circle
+        cx={centerX1}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorLeft}
+        strokeWidth={strokeWidth}
+      />
+      <circle
+        cx={centerX2}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorRight}
+        strokeWidth={strokeWidth}
+      />
     </svg>
-  );
+  )
 }
 
 export const LeftJoinSVG = () => {
@@ -43,10 +57,24 @@ export const LeftJoinSVG = () => {
       <title>Left join</title>
       <circle cx={centerX1} cy={centerY} r={radius} fill={fillColor} />
       {/* Borders always on top */}
-      <circle cx={centerX1} cy={centerY} r={radius} fill="none" stroke={strokeColorLeft} strokeWidth={strokeWidth} />
-      <circle cx={centerX2} cy={centerY} r={radius} fill="none" stroke={strokeColorRight} strokeWidth={strokeWidth} />
+      <circle
+        cx={centerX1}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorLeft}
+        strokeWidth={strokeWidth}
+      />
+      <circle
+        cx={centerX2}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorRight}
+        strokeWidth={strokeWidth}
+      />
     </svg>
-  );
+  )
 }
 
 export const RightJoinSVG = () => {
@@ -55,10 +83,24 @@ export const RightJoinSVG = () => {
       <title>Right join</title>
       <circle cx={centerX2} cy={centerY} r={radius} fill={fillColor} />
       {/* Borders always on top */}
-      <circle cx={centerX1} cy={centerY} r={radius} fill="none" stroke={strokeColorLeft} strokeWidth={strokeWidth} />
-      <circle cx={centerX2} cy={centerY} r={radius} fill="none" stroke={strokeColorRight} strokeWidth={strokeWidth} />
+      <circle
+        cx={centerX1}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorLeft}
+        strokeWidth={strokeWidth}
+      />
+      <circle
+        cx={centerX2}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorRight}
+        strokeWidth={strokeWidth}
+      />
     </svg>
-  );
+  )
 }
 
 export const OuterJoinSVG = () => {
@@ -68,8 +110,22 @@ export const OuterJoinSVG = () => {
       <circle cx={centerX1} cy={centerY} r={radius} fill={fillColor} />
       <circle cx={centerX2} cy={centerY} r={radius} fill={fillColor} />
       {/* Borders always on top */}
-      <circle cx={centerX1} cy={centerY} r={radius} fill="none" stroke={strokeColorLeft} strokeWidth={strokeWidth} />
-      <circle cx={centerX2} cy={centerY} r={radius} fill="none" stroke={strokeColorRight} strokeWidth={strokeWidth} />
+      <circle
+        cx={centerX1}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorLeft}
+        strokeWidth={strokeWidth}
+      />
+      <circle
+        cx={centerX2}
+        cy={centerY}
+        r={radius}
+        fill="none"
+        stroke={strokeColorRight}
+        strokeWidth={strokeWidth}
+      />
     </svg>
-  );
+  )
 }
